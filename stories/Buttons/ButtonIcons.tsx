d445f44605f1d832b5c2dfac0b5a243e46b26a6d
ethan <ksr20612@daum.net>
2023-10-03 08:59:01 +0900

    init

--- stories/Buttons/ButtonIcons.tsx
+++
@@ -1,9 +0,0 @@
-import HeartIcon from 'public/icons/heart.svg';
-import PlusIcon from 'public/icons/plus.svg';
-
-const ButtonIcons = {
-  heart: <HeartIcon />,
-  plus: <PlusIcon />,
-};
-
-export default ButtonIcons;
